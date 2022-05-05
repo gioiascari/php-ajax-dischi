2493731f95f4b589e1f4e8e5cfac910ae6b616a0
gioiascari <gioia.ascari96@gmail.com>
2022-05-05 16:07:09 +0200

    sistemazione layout, milestone 1 completata

--- data.php
+++ data.php
@@ -81,7 +81,23 @@
                 'year'=> "1987",
 
             ],
+            [
+                'author'=>"Bon Jovi",
+                'genre'=> "Rock",
+                'poster'=> "https://www.onstageweb.com/wp-content/uploads/2018/09/bon-jovi-new-jersey.jpg",
+                'title'=> "New Jersey",
+                'year'=> "1988",
+
+            ],
+            [
+                'author'=>"Queen",
+                'genre'=> "Pop",
+                'poster'=> "https://img.discogs.com/vknPDdrqRbT92pNRX0W4I5N91jg=/fit-in/300x300/filters:strip_icc():format(jpeg):mode_rgb():quality(40)/discogs-images/R-1246953-1448927086-6590.jpeg.jpg",
+                'title'=> "Live at Wembley 86",
+                'year'=> "1992",
+
+            ],
         ];
-    header('Content-Type:application/json');
-    echo json_encode($dbDischi);
+    // header('Content-Type:application/json');
+    // echo json_encode($dbDischi);
 ?>
--- index.php
+++ index.php
@@ -10,24 +10,60 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    
+    <!-- Bootstrap -->
+    <link
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
+      rel="stylesheet"
+      integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
+      crossorigin="anonymous"
+    />
+    <!-- /Bootstrap -->
+    <link rel="stylesheet" href="css/style.css" />
+    <!-- Vue -->
+    <script src="https://cdn.jsdelivr.net/npm/vue@2/dist/vue.js"></script>
+    <!-- /Vue -->
     <title>PHP Dischi</title>
 </head>
 <body>
-<?php 
-        include('data.php');
-            foreach($dbDischi as $dischi=>$detail){?>
-            <div class="card_dischi">
-            <img src="<?php echo $detail['poster'];?>">
-            <h1> <?php echo $detail['author'];?></h1>
-            <p> <?php echo $detail['title'];?></p>
-
+    <header>
+        <div class="container-fluid">
+            <div class="row ">
+                <div class="col-12">
+                    <div class="logo-img w-50 p-2">
+                    <img src="https://storage.googleapis.com/pr-newsroom-wp/1/2018/11/Spotify_Logo_RGB_Green.png" alt="" class="w-25">
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+    </header>
+    <main>
+        <div class="container padding_top">
+            <div class="row">
+                
+                      <?php 
+                include('data.php');
+                     foreach($dbDischi as $dischi=>$detail){?>
+            <div class="img_container col-3">
+                <img src="<?php echo $detail['poster'];?>" class="w-100 ">
+                    <h1 class="font_size"> <?php echo $detail['author'];?></h1>
+                 <div class="text_card">
+                    <p> <?php echo $detail['title'];?></p>
+                </div>
 
             </div>
 
         <?php 
         }
         ?>
+                
+            </div>
 
+          
+        </div>
+    
+    </main>
+
+ <script src="js/main.js"></script>
 </body>
 </html>
