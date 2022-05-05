aeb3ca2e3a1fc14b2b07b8d7c853a279dff4fcb2
gioiascari <gioia.ascari96@gmail.com>
2022-05-05 14:54:48 +0200

    impostazione milestone 1 riuscita

--- index.php
+++ index.php
@@ -1,6 +1,33 @@
 <?php 
-    include('data.php');
-    
+    // include('data.php');
     
 
+
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <title>PHP Dischi</title>
+</head>
+<body>
+<?php 
+        include('data.php');
+            foreach($dbDischi as $dischi=>$detail){?>
+            <div class="card_dischi">
+            <img src="<?php echo $detail['poster'];?>">
+            <h1> <?php echo $detail['author'];?></h1>
+            <p> <?php echo $detail['title'];?></p>
+
+
+            </div>
+
+        <?php 
+        }
+        ?>
+
+</body>
+</html>
